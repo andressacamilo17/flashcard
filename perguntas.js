08fbbe5507094ba6682bd601eda4c0617b3be511
andressacamilo17 <159184328+andressacamilo17@users.noreply.github.com>
2024-10-08 16:50:42 -0300

    Evet veya haír

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'Programação',
+    'O que python',
+    ' O python é uma linguagem de programação'
+)
+criaCartao(
+    'Geografia',
+    ' Qualé a capital da França',
+    ' a capital da França é Paris'
+)
+criaCartao(
+    'Programação',
+    'Qual é a função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
